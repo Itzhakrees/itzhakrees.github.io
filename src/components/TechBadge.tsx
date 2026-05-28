ed077bd6111b0a3ab1e5872fd37e472031ce315b
MOTHERBASE <773844738@qq.com>
2026-05-28 17:31:27 +0800

    # Init new branch for template

--- src/components/TechBadge.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "@/styles/Portfolio.module.css";
-
-type Props = {
-  label: string;
-};
-
-export default function TechBadge({ label }: Props) {
-  return <span className={styles.techBadge}>{label}</span>;
-}
